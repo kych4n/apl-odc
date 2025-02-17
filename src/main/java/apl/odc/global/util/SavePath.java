e9640a09a4fcf6137af7d35887efd7a5239658bf
kych4n <hades1@kakao.com>
2025-02-18 00:06:53 +0900

    Rename [#8] DataPath -> SavePath

--- src/main/java/apl/odc/global/util/SavePath.java
+++ src/main/java/apl/odc/global/util/SavePath.java
@@ -3,13 +3,13 @@ package apl.odc.global.util;
 import lombok.Getter;
 
 @Getter
-public enum DataPath {
+public enum SavePath {
     ORIGINAL("src/main/resources/data/original_data.csv"),
     FILTERED("src/main/resources/data/filtered_data.csv");
 
     private final String path;
 
-    DataPath(String path) {
+    SavePath(String path) {
         this.path = path;
     }
 }
