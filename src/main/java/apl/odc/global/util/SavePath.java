146059d511e26a717317e84a471f2511fc5b054a
kych4n <hades1@kakao.com>
2025-02-19 22:40:27 +0900

    Feat [#8] 도커 환경을 고려한 경로 수정 및 추가

--- src/main/java/apl/odc/global/util/SavePath.java
+++ src/main/java/apl/odc/global/util/SavePath.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum SavePath {
-    ORIGINAL("src/main/resources/data/original_data.csv"),
-    FILTERED("src/main/resources/data/filtered_data.csv"),
-    FRAME("C:/filtering/src/main/java/apl/filtering/frame"),
-    RULE("C:/filtering/src/main/resources/rules");
+    ORIGINAL("/app/data/original_data.csv"),
+    FILTERED("/app/data/filtered_data.csv"),
+    FRAME("/app/filtering/src/main/java/apl/filtering/frame"),
+    RULE("/app/filtering/src/main/resources/rules"),
+    JSON("/app/data/mappingInfo.json"),
+    NEW_BUILD("/app/filtering");
 
     private final String path;
 
