9bc3bf9ff09c851d9663cd581b59e6a74a898715
kych4n <hades1@kakao.com>
2025-02-19 23:34:31 +0900

    Feat [#8] 필터링 프로젝트 빌드 및 실행

--- src/main/java/apl/odc/api/facade/MainFacade.java
+++ src/main/java/apl/odc/api/facade/MainFacade.java
@@ -1,15 +1,20 @@
 package apl.odc.api.facade;
 
 import apl.odc.api.dto.request.InfoForFilteringRequest;
+import apl.odc.api.dto.request.MappingInfoDTO;
 import apl.odc.api.dto.response.AttributeResponse;
 import apl.odc.global.util.CSVFileReader;
 import apl.odc.global.util.FileDownloader;
 import apl.odc.global.util.SavePath;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MainFacade {
@@ -25,5 +30,41 @@ public class MainFacade {
     public void prepareForFiltering(InfoForFilteringRequest infoForFilteringRequest) throws IOException {
         fileDownloader.downloadFiles(infoForFilteringRequest.frameUrl(), SavePath.FRAME.getPath());
         fileDownloader.downloadFiles(infoForFilteringRequest.ruleUrl(), SavePath.RULE.getPath());
+
+        convert(infoForFilteringRequest.mappingInfo());
     }
+
+    public void filter() {
+        try {
+            ProcessBuilder buildProcessBuilder = new ProcessBuilder("./gradlew", "clean", "build");
+            buildProcessBuilder.directory(new java.io.File(SavePath.NEW_BUILD.getPath()));
+            buildProcessBuilder.inheritIO();
+
+            Process buildProcess = buildProcessBuilder.start();
+            int buildExitCode = buildProcess.waitFor();
+
+            if (buildExitCode == 0) {
+                ProcessBuilder runProcessBuilder = new ProcessBuilder("java", "-jar",
+                        SavePath.NEW_BUILD.getPath() + "/build/libs/filtering.jar");
+                runProcessBuilder.inheritIO();
+                Process process = runProcessBuilder.start();
+                process.waitFor();
+            } else {
+                log.info("Filtering Failed.");
+            }
+
+        } catch (IOException | InterruptedException e) {
+            log.info(e.getMessage());
+        }
+    }
+
+    private void convert(MappingInfoDTO mappingInfoDTO) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File(SavePath.JSON.getPath()), mappingInfoDTO);
+        } catch (IOException e) {
+            log.info(e.getMessage());
+        }
+    }
+    
 }
