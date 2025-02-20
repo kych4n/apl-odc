b70bb3c26bee9e507d8f2a59db370d2ea7626f24
kych4n <hades1@kakao.com>
2025-02-20 17:01:27 +0900

    Feat [#8] 암호화 객체 생성

---
+++ src/main/java/apl/odc/global/util/Encryptor.java
@@ -0,0 +1,48 @@
+package apl.odc.global.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class Encryptor {
+
+    public void encrypt(SecretKey secretKey) throws Exception {
+        File inputFile = new File(SavePath.FILTERED.getPath());
+        File outputFile = new File(SavePath.ENCRYPTED.getPath());
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+        try (FileInputStream fis = new FileInputStream(inputFile);
+             FileOutputStream fos = new FileOutputStream(outputFile);
+             CipherOutputStream cos = new CipherOutputStream(fos, cipher)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                cos.write(buffer, 0, bytesRead);
+            }
+        }
+    }
+
+    public SecretKey generateKey(Long userId) throws NoSuchAlgorithmException {
+        MessageDigest sha = MessageDigest.getInstance("SHA-256");
+        byte[] key = sha.digest(userId.toString().getBytes(StandardCharsets.UTF_8));
+
+        return new SecretKeySpec(Arrays.copyOf(key, 16), "AES");
+    }
+
+}
