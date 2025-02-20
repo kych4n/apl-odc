146544d4c7f3df34eed6fa5ddd3b8db6ffdbc41e
kych4n <hades1@kakao.com>
2025-02-20 17:02:09 +0900

    Feat [#8] S3 핸들러 생성

---
+++ src/main/java/apl/odc/global/util/s3/S3PropertiesConfiguration.java
@@ -0,0 +1,9 @@
+package apl.odc.global.util.s3;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan(basePackages = "apl.odc.global.util.s3")
+public class S3PropertiesConfiguration {
+}
