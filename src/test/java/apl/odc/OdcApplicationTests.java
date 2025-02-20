a6ad9554c1593fce2f9923e8b81c191f13c083ad
kych4n <hades1@kakao.com>
2025-02-20 17:05:44 +0900

    BugFix [#8] 빌드 에러를 발생시키는 테스트 제외

--- src/test/java/apl/odc/OdcApplicationTests.java
+++ src/test/java/apl/odc/OdcApplicationTests.java
@@ -1,13 +1,11 @@
 package apl.odc;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class OdcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
